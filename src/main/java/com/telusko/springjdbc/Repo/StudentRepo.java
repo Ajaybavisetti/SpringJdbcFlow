13caa307a2663fad1d486ca89522e40bf3ea515e
Ajaybavisetti <ajaykumarbavisetti@gmail.com>
2026-03-30 21:41:57 +0530

    Initial project commit

--- src/main/java/com/telusko/springjdbc/Repo/StudentRepo.java
+++ src/main/java/com/telusko/springjdbc/Repo/StudentRepo.java
@@ -1,20 +1,49 @@
 package com.telusko.springjdbc.Repo;
 
 import com.telusko.springjdbc.Model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository
 public class StudentRepo {
+    public JdbcTemplate getJdbc() {
+        return jdbc;
+    }
+    @Autowired
+    public void setJdbc(JdbcTemplate jdbc) {
+        this.jdbc = jdbc;
+    }
+
+    private JdbcTemplate jdbc;
+
     public void save(Student s) {
-        System.out.println("StudentRepo save");
+        String sql="insert into Student(rollno,name,marks) values(?,?,?)";
+        int rows=jdbc.update(sql,s.getRollno(),s.getName(),s.getMarks());
+        System.out.println("rows Effected:"+rows);
     }
 
 
     public List<Student> findAll() {
-        List<Student> students = new ArrayList<>();
-        return students;
+        String sql="select * from Student";
+        RowMapper<Student> mapper=new  RowMapper<Student>() {
+
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Student s=new Student();
+                s.setRollno(rs.getInt("rollno"));
+                s.setName(rs.getString("name"));
+                s.setMarks(rs.getInt("marks"));
+
+                return s;
+            }
+        };
+        return jdbc.query(sql,mapper);
     }
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into Student(rollno,name,marks) values (101,'Ajay',78);
+insert into Student(rollno,name,marks) values (102,'Dinesh',68);
+insert into Student(rollno,name,marks) values (103,'Neha',82);
